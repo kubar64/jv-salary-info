72ab024e1784d56c2011d5bfb445dfc154f80f4b
Jakub <kuba.r.pawlowski@gmail.com>
2023-12-05 01:53:55 +0100

    I did it!

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,32 +1,38 @@
 package core.basesyntax;
 
 import java.text.ParseException;
-import java.util.Arrays;
 import java.util.Date;
 
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo)  {
-
+    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringToDate stringToDate = new StringToDate();
-        StringBuilder stringBuilder = new StringBuilder("Report for period "+dateFrom+" - "+dateTo);
-        stringBuilder.append(System.getProperty("line.separator"));
+        StringBuilder stringBuilder = new StringBuilder("Report for period "
+                + dateFrom
+                + " - "
+                + dateTo);
         try {
             Date start = stringToDate.stringToDate(dateFrom);
             Date end = stringToDate.stringToDate(dateTo);
             for (String name:
-                 names) {
+                    names) {
                 int salary = 0;
-                for (int i = 0; i < data.length ; i++){
-                String[] record = data[i].split(" ");
-                Date actual = stringToDate.stringToDate(record[0]);
-                if (name.equals(record[1]) && (actual.after(start) || actual.equals(start)) && (actual.before(end) || actual.equals(end))){
-                    salary += Integer.valueOf(record[2])*Integer.valueOf(record[3]);
+                for (String datum : data) {
+                    String[] record = datum.split(" ");
+                    Date actual = stringToDate.stringToDate(record[0]);
+                    if (name.equals(record[1])
+                            && (actual.after(start) || actual.equals(start))
+                            && (actual.before(end) || actual.equals(end))) {
+                        salary += Integer.parseInt(record[2]) * Integer.parseInt(record[3]);
+                    }
                 }
-                }
-            stringBuilder.append(name).append(" - ").append(salary).append(System.getProperty("line.separator"));
+                stringBuilder.append(System.getProperty("line.separator"))
+                                .append(name)
+                                .append(" - ")
+                                .append(salary);
             }
 
-        }catch(ParseException ignored){
+        } catch (ParseException ignore) {
+            super.notifyAll();
         }
         return stringBuilder.toString();
     }
